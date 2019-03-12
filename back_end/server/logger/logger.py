518449307b2f9cdfd026fa8edba66a2a41d273de
vladislavvovk <vladislav.vovk@nextiva.com>
2019-03-12 11:59:10 +0200

    - added simple logger module - started db part

---
+++ back_end/server/logger/logger.py
@@ -0,0 +1,41 @@
+from termcolor import colored
+
+class logger:
+
+    SUPPORTED_COLORS = (
+        'grey',
+        'red',
+        'green',
+        'yellow',
+        'blue',
+        'magenta',
+        'cyan',
+        'white'
+    )
+
+    SUPPORTED_ATTRIBUTES = (
+        'bold',
+        'dark',
+        'underline',
+        'blink',
+        'reverse',
+        'concealed',
+    )
+
+    def __init__(self, module_name, color, delimiter='-->'):
+        self.name = module_name
+        self.delimiter = delimiter
+        if color not in self.SUPPORTED_COLORS:
+            raise ValueError('Given color is not supported')
+        else:
+            self.color = color
+
+    def log(self, message, attribute=None):
+        if message == None:
+            warning = colored('Message is not passed to the function', 'yellow', 'bold')
+            print(warning)
+
+        else:
+            text = f'{self.delimiter}{self.name}{message}'
+            log_message = colored(text, self.color, None, attribute)
+            print(log_message)
