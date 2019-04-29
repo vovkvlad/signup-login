bbab55e383c7f207e8de86867508dba4fd4cbcfc
vovkvlad <VKolo.bokV@gmail.com>
2019-04-29 17:45:20 +0300

    - added password hashing and quthentication method

--- back_end/server/db/user.py
+++ back_end/server/db/user.py
@@ -1,3 +1,4 @@
+import bcrypt
 from server.db import get_db, db_logger
 
 
@@ -6,16 +7,30 @@ def create_user(email, password):
         raise ValueError('Email and password a mandatory fields')
 
     db_logger.log(f'Trying to create user with email={email}, password={password}')
+
+    hashed_passwd = bcrypt.hashpw(password, bcrypt.gensalt())
+
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("INSERT INTO users(email, password) VALUES (?,?)", (email, password))
+    cursor.execute("INSERT INTO users(email, password) VALUES (?,?)", (email, hashed_passwd))
 
     new_user_id = cursor.lastrowid
 
-
     db.commit()
-    db_logger(f'User has been created successfully. New user_id is {new_user_id}')
+    db_logger.log(f'User has been created successfully. New user_id is {new_user_id}')
     return new_user_id
+
+
+def authenticate_user(email, password):
+    [id, stored_email, hashed_password] = find_user(email)
+
+    if stored_email is None:
+        raise LookupError('Invalid email or password')
+
+    if bcrypt.hashpw(password, hashed_password) == hashed_password:
+        return {'id': id, 'email': stored_email}
+    else:
+        raise LookupError('Invalid email or password')
 
 
 def find_user(email):
--- back_end/server/routes.py
+++ back_end/server/routes.py
@@ -1,5 +1,5 @@
 from flask import send_from_directory, send_file
-from server.db.user import create_user, find_user
+from server.db.user import create_user, authenticate_user
 
 def register_routes(app):
     @app.route('/')
@@ -18,13 +18,18 @@ def register_routes(app):
     def host_static(path):
         return send_from_directory('../../front-end/public', path)
 
-    #this is demo route - should be deleted later
+    #this is demo routes - should be deleted later
     @app.route('/user/create')
     def create_user_request():
-        create_user("testemailAAAAA@gamil.com", "password123")
+        create_user('testemailAAAAA@gamil.com', 'password123')
         return 'User should have been created'
-        #created_user = find_user('testemail@gamil.com')
 
-        # print("=========== CREATED USER ===============")
-        # print(f"{created_user}")
-        # print("=========== CREATED USER ===============")
+    @app.route('/user/auth')
+    def authenticate_user_request():
+        try:
+            user = authenticate_user('testemailAAAAA@gamil.com', 'password12344')
+            return user['email']
+
+        except LookupError as err:
+            print(err)
+            return err.args[0]
